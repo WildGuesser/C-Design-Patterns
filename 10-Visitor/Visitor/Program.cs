92b3efee1571eff33a8b39a03f1d84479d74bcf2
unknown <hugomansilha@gmail.com>
2025-04-27 17:46:39 +0100

    Visitor and Memento Added

---
+++ 10-Visitor/Visitor/Program.cs
@@ -0,0 +1,153 @@
+﻿namespace VisitorPatternExample
+{
+    using System;
+    using System.Collections.Generic;
+
+    // Visitor interface
+    public interface IVisitor
+    {
+        void VisitMenu(Menu menu);
+        void VisitMenuItem(MenuItem menuItem);
+        void VisitIngredient(Ingredient ingredient);
+    }
+
+    // Element interface
+    public interface IVisitable
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    // Concrete Elements
+    public class Menu : IVisitable
+    {
+        public List<IVisitable> Items { get; } = new List<IVisitable>();
+        public string Name { get; }
+
+        public Menu(string name)
+        {
+            Name = name;
+        }
+
+        public void AddItem(IVisitable item)
+        {
+            Items.Add(item);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitMenu(this);
+            foreach (var item in Items)
+            {
+                item.Accept(visitor);
+            }
+        }
+    }
+
+    public class MenuItem : IVisitable
+    {
+        public string Name { get; }
+        public List<Ingredient> Ingredients { get; } = new List<Ingredient>();
+
+        public MenuItem(string name)
+        {
+            Name = name;
+        }
+
+        public void AddIngredient(Ingredient ingredient)
+        {
+            Ingredients.Add(ingredient);
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitMenuItem(this);
+            foreach (var ingredient in Ingredients)
+            {
+                ingredient.Accept(visitor);
+            }
+        }
+    }
+
+    public class Ingredient : IVisitable
+    {
+        public string Name { get; }
+        public int Calories { get; }
+        public int Protein { get; }
+        public int Carbs { get; }
+
+        public Ingredient(string name, int calories, int protein, int carbs)
+        {
+            Name = name;
+            Calories = calories;
+            Protein = protein;
+            Carbs = carbs;
+        }
+
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitIngredient(this);
+        }
+    }
+
+    // Concrete Visitor
+    public class NutritionVisitor : IVisitor
+    {
+        public int TotalCalories { get; private set; }
+        public int TotalProtein { get; private set; }
+        public int TotalCarbs { get; private set; }
+
+        public void VisitMenu(Menu menu)
+        {
+            Console.WriteLine($"Visiting Menu: {menu.Name}");
+        }
+
+        public void VisitMenuItem(MenuItem menuItem)
+        {
+            Console.WriteLine($"  Visiting Menu Item: {menuItem.Name}");
+        }
+
+        public void VisitIngredient(Ingredient ingredient)
+        {
+            Console.WriteLine($"    Visiting Ingredient: {ingredient.Name} (Calories: {ingredient.Calories}, Protein: {ingredient.Protein}, Carbs: {ingredient.Carbs})");
+            TotalCalories += ingredient.Calories;
+            TotalProtein += ingredient.Protein;
+            TotalCarbs += ingredient.Carbs;
+        }
+    }
+
+    // Client
+    class Program
+    {
+        static void Main()
+        {
+            // Criar ingredientes
+            Ingredient egg = new Ingredient("Egg", 70, 6, 1);
+            Ingredient bacon = new Ingredient("Bacon", 100, 7, 0);
+            Ingredient pancake = new Ingredient("Pancake", 150, 4, 20);
+
+            // Criar menu items
+            MenuItem breakfastSpecial = new MenuItem("Breakfast Special");
+            breakfastSpecial.AddIngredient(egg);
+            breakfastSpecial.AddIngredient(bacon);
+
+            MenuItem sweetBreakfast = new MenuItem("Sweet Breakfast");
+            sweetBreakfast.AddIngredient(pancake);
+
+            // Criar menu
+            Menu breakfastMenu = new Menu("Breakfast Menu");
+            breakfastMenu.AddItem(breakfastSpecial);
+            breakfastMenu.AddItem(sweetBreakfast);
+
+            // Criar Visitor
+            NutritionVisitor nutritionVisitor = new NutritionVisitor();
+
+            // Aplicar o Visitor
+            breakfastMenu.Accept(nutritionVisitor);
+
+            Console.WriteLine("\nTotal Nutritional Information:");
+            Console.WriteLine($"Calories: {nutritionVisitor.TotalCalories}");
+            Console.WriteLine($"Protein: {nutritionVisitor.TotalProtein}g");
+            Console.WriteLine($"Carbs: {nutritionVisitor.TotalCarbs}g");
+        }
+    }
+}
---
+++ 9-Memento/Memento/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace MementoPattern
+{
+    using System;
+
+    // Originator
+    public class TextEditor
+    {
+        private string _text;
+
+        public void Type(string words)
+        {
+            _text += words;
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+
+        public Memento Save()
+        {
+            return new Memento(_text);
+        }
+
+        public void Restore(Memento memento)
+        {
+            _text = memento.GetSavedText();
+        }
+    }
+
+    // Memento
+    public class Memento
+    {
+        private readonly string _text;
+
+        public Memento(string text)
+        {
+            _text = text;
+        }
+
+        public string GetSavedText()
+        {
+            return _text;
+        }
+    }
+
+    // Caretaker
+    public class History
+    {
+        private Stack<Memento> _history = new Stack<Memento>();
+
+        public void SaveState(TextEditor editor)
+        {
+            _history.Push(editor.Save());
+        }
+
+        public void Undo(TextEditor editor)
+        {
+            if (_history.Count > 0)
+            {
+                Memento memento = _history.Pop();
+                editor.Restore(memento);
+            }
+        }
+    }
+
+    // Client
+    class Program
+    {
+        static void Main()
+        {
+            TextEditor editor = new TextEditor();
+            History history = new History();
+
+            editor.Type("Hello, ");
+            history.SaveState(editor);
+
+            editor.Type("world!");
+            Console.WriteLine(editor.GetText()); // Output: Hello, world!
+
+            history.Undo(editor);
+            Console.WriteLine(editor.GetText()); // Output: Hello, 
+        }
+    }
+}
